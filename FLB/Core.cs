4810b0aafaf5c3bc48586666462309837c6ba22c
HAHOOS <hahoos@hahoos.dev>
2026-05-06 01:35:32 +0200

    chore: bump version and add assembly info

--- FLB/Core.cs
+++ FLB/Core.cs
@@ -1,27 +1,13 @@
-﻿using System;
-using System.Linq;
-using System.Collections;
-using System.Text.RegularExpressions;
+﻿using MelonLoader;
 
-using FLB;
-
-using Il2CppSLZ.Marrow.SceneStreaming;
-
-using LabFusion.Data;
-using LabFusion.Network;
-using LabFusion.UI.Popups;
-
-using MelonLoader;
 using FLB.Managers;
-
-[assembly: MelonInfo(typeof(Core), "FLB", "1.0.1", "HAHOOS", null)]
-[assembly: MelonGame("Stress Level Zero", "BONELAB")]
-[assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
 
 namespace FLB
 {
     public class Core : MelonMod
     {
+        public const string Version = "1.1.0";
+
         internal static MelonLogger.Instance Logger { get; private set; }
 
         internal static Core Instance { get; private set; }
